23558fb0c02c96f151ca8d3279448776de632f20
Noah Gift <noah.gift@gmail.com>
2022-12-06 20:37:33 +0000

    adding azure config

--- LocalFunctionProj/function_app.py
+++ LocalFunctionProj/function_app.py
@@ -5,4 +5,4 @@ app = func.FunctionApp()
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("HttpTrigger1 function processed a request!")%
+    return func.HttpResponse("YAGNI:  CONDA YOU DON'T NEED CONDA!!!")%
